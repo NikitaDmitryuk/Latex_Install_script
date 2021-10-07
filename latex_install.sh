014f69d90cd517a39f1376ba26a5f82cc04fd214
Никита <50713261+NikitaDmitryuk@users.noreply.github.com>
2021-10-07 15:08:32 +0300

    Update latex_install.sh
    update package

--- latex_install.sh
+++ latex_install.sh
@@ -2,13 +2,6 @@
 
 sudo pacman -Syu
 
-sudo pacman -S --noconfirm texlive-bin
-sudo pacman -S --noconfirm texlive-core
-sudo pacman -S --noconfirm texlive-langcyrillic
-sudo pacman -S --noconfirm texlive-publishers
-sudo pacman -S --noconfirm pydflatex
+sudo pacman -S --noconfirm texlive-bin texlive-core texlive-langcyrillic texlive-publishers texlive-bibtexextra texlive-fontsextra texlive-latexextra
+sudo pacman -S --noconfirm pydflatex biber kbibtex
 sudo pacman -S --noconfirm kile
-sudo pacman -S --noconfirm biber
-sudo pacman -S --noconfirm texlive-bibtexextra
-sudo pacman -S --noconfirm texlive-fontsextra
-sudo pacman -S --noconfirm kbibtex
