6078223b70905f82bef9bd27e6d415881a4240bf
Никита <50713261+NikitaDmitryuk@users.noreply.github.com>
2021-11-10 11:39:27 +0300

    Update pac

--- latex_install.sh
+++ latex_install.sh
@@ -2,6 +2,6 @@
 
 sudo pacman -Syu
 
-sudo pacman -S --noconfirm texlive-bin texlive-core texlive-langcyrillic texlive-publishers texlive-bibtexextra texlive-fontsextra texlive-latexextra texlive-most ttf-ms-fonts
+sudo pacman -S --noconfirm texlive-science texlive-bin texlive-core texlive-langcyrillic texlive-publishers texlive-bibtexextra texlive-fontsextra texlive-latexextra texlive-most ttf-ms-fonts
 sudo pacman -S --noconfirm pydflatex biber kbibtex
 sudo pacman -S --noconfirm kile
