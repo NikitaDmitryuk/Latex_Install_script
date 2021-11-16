553928a01f95fcdbcd0ddea058d761b99aee9e93
Никита <50713261+NikitaDmitryuk@users.noreply.github.com>
2021-11-16 12:25:35 +0300

    Update

--- latex_install.sh
+++ latex_install.sh
@@ -2,5 +2,9 @@
 
 sudo pacman -Syu
 
-sudo pacman -S --noconfirm texlive-most pydflatex biber kbibtex kile
+sudo pacman -S --noconfirm texlive-most
+sudo pacman -S --noconfirm pydflatex 
+sudo pacman -S --noconfirm biber
+sudo pacman -S --noconfirm kbibtex 
+sudo pacman -S --noconfirm kile
 sudo pacman -S --noconfirm ttf-ms-fonts
